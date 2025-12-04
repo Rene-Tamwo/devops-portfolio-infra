08aa31103b587d1326a2101009bd0da699ce9f3f
René Tamwo <tamwotagne@gmail.com>
2025-12-04 11:51:37 +0100

    Nouvelle Ajout

--- terraform/main.tf
+++ terraform/main.tf
@@ -200,11 +200,10 @@ resource "aws_instance" "workers" {
 
 data "aws_ami" "ubuntu" {
   most_recent = true
-  owners      = ["099720109477"] # Canonical
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-22.04-*-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
   }
 
   filter {
@@ -212,10 +211,6 @@ data "aws_ami" "ubuntu" {
     values = ["hvm"]
   }
 
-  filter {
-    name   = "architecture"
-    values = ["x86_64"]
-  }
+  owners = ["099720109477"] # Canonical
 }
-
 
