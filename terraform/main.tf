32904a633f44e5dc2b90b0ac7def287783ad1c02
René Tamwo <tamwotagne@gmail.com>
2025-12-23 14:34:59 +0100

    Nouvel Ajout

--- terraform/main.tf
+++ terraform/main.tf
@@ -204,17 +204,12 @@ data "aws_ami" "ubuntu" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-22.04-*-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-22.04-*-server-*"]
   }
 
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
-  }
-
-  filter {
-    name   = "architecture"
-    values = ["x86_64"]
   }
 }
 
